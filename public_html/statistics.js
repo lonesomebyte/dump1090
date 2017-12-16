ef034a303888f8c15b8c55753818afddd7c7831b
Lonesome Byte <lonesome.byte@gmail.com>
2017-12-16 17:56:03 +0100

    Speed statistics
    Statistics pane now shows statistical information of the speed of
    all planes.

--- public_html/statistics.js
+++ public_html/statistics.js
@@ -29,10 +29,13 @@ liveStatisticsProvider = {
         var altitudes = [];
         var distances = [];
         var airlines = [];
+        var speeds = [];
         var maxDistance = undefined;
         var minDistance = undefined;
         var maxAltitude = undefined;
         var minAltitude = undefined;
+        var maxSpeed = undefined;
+        var minSpeed = undefined;
         for (var hex in planes) {
             var icao = planes[hex].icao;
             types.push(icao in planeTypes?planeTypes[icao]:icao);
@@ -50,17 +53,27 @@ liveStatisticsProvider = {
                 distance = (parseInt(distance / div)+1)*div;
                 distances.push(distance);
             }
+            if (!isNaN(planes[hex].speed)) {
+                var speed = parseInt(planes[hex].speed);
+                if (maxSpeed===undefined || speed>maxSpeed) {maxSpeed=speed;} 
+                if (minSpeed===undefined || speed<minSpeed) {minSpeed=speed;} 
+                var div=40;
+                speed = (parseInt(speed / div)+1)*div;
+                speeds.push(speed);
+            }
             airlines.push(planes[hex].airline);
         }
         altitudes = dictToArray(arrayToCount(altitudes),'altitude','count','altitude');
         distances = dictToArray(arrayToCount(distances),'distance','count','distance');
+        speeds = dictToArray(arrayToCount(speeds),'speed','count','speed');
         airlines = arrayToCount(airlines);
         var airlineCount = Object.keys(airlines).length;
         airlines = dictToArray(airlines,'airline','count','count');
         var statistics = {'planeTypes':{'total':Object.keys(planes).length, 'types':dictToArray(arrayToCount(types),'type','count', 'count')},
                           'airlines':{'total':airlineCount, 'airlines':airlines},
                           'altitudes':{'highest':maxAltitude?maxAltitude:'---', 'lowest':minAltitude?minAltitude:'---', 'altitudes':altitudes},
-                          'distances':{'farthest':maxDistance?maxDistance:'---', 'closest':minDistance?minDistance:'---', 'distances':distances}};
+                          'distances':{'farthest':maxDistance?maxDistance:'---', 'closest':minDistance?minDistance:'---', 'distances':distances},
+                          'speeds':{'fastest':maxSpeed?maxSpeed:'---', 'slowest':minSpeed?minSpeed:'---', 'speeds':speeds}};
         cb(statistics);
     }
 }
@@ -118,6 +131,13 @@ function showStatistics() {
                         "<div class='pane'>"+
                             "<table class='centered'><tr><th class='big'>Highest</th><th class='big'>Lowest</th></tr><tr><td class='number big' data-field='highest'></td><td class='number big' data-field='lowest'></td></tr></table>"+
                             "<table class='list' data-field='altitudes'><tr><th>#</th><th>Altitude</th></tr><tr class='list-item'><td class='number' data-field='count'></td><td><span data-field='altitude'></span> ft</td></tr></table>"+
+                        "</div>"));
+                    break;
+                case 'speeds':
+                    addPane(statistic, $(
+                        "<div class='pane'>"+
+                            "<table class='centered'><tr><th class='big'>Fastest</th><th class='big'>Slowest</th></tr><tr><td class='number big' data-field='fastest'></td><td class='number big' data-field='slowest'></td></tr></table>"+
+                            "<table class='list' data-field='speeds'><tr><th>#</th><th>Speed</th></tr><tr class='list-item'><td class='number' data-field='count'></td><td><span data-field='speed'></span> kts</td></tr></table>"+
                         "</div>"));
                     break;
                 case 'distances':
